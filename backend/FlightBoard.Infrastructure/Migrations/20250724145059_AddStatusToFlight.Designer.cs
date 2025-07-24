572c0cad5b307b3dcdda3da2699c924be6536896
Shani <finorshani@gmail.com>
2025-07-24 18:06:09 +0300

    DB update

--- backend/FlightBoard.Infrastructure/Migrations/20250724145059_AddStatusToFlight.Designer.cs
+++ backend/FlightBoard.Infrastructure/Migrations/20250724145059_AddStatusToFlight.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FlightBoard.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlightBoard.Infrastructure.Migrations
 {
     [DbContext(typeof(FlightDbContext))]
-    partial class FlightDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250724145059_AddStatusToFlight")]
+    partial class AddStatusToFlight
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
